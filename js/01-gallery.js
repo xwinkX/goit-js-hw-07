fb3d3a9c14b6c2ae6f7078669f88e4ee70a42669
xwinkX <opalyuk.e88@ukr.net>
2022-05-19 22:54:22 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// Change code below this line
+const imageGallery = document.querySelector('.gallery');
+
+const galleryMarkup = createGallery(galleryItems);
+imageGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+imageGallery.addEventListener('click', galleryElementClick);
+
+function createGallery(images) {
+        return images
+        .map(({ preview, original, description }) => {
+            return `
+            <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+             alt= "${description}"
+        />
+     </a>
+    </div>
+    `;
+        })
+        .join('');
+    
+}
+
+function galleryElementClick(event) {
+    event.preventDefault();
+    console.log(event.target);
+} 
 console.log(galleryItems);
+
