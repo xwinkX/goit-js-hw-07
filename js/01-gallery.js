d645b933ba8ec630fa0ee876d27901171a59406f
xwinkX <opalyuk.e88@ukr.net>
2022-05-21 21:15:55 +0300

    update1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
 const imageGallery = document.querySelector('.gallery');
-
+// const imageGalleryClose = document.querySelector('.basicLightbox');
 const galleryMarkup = createGallery(galleryItems);
 imageGallery.insertAdjacentHTML('beforeend', galleryMarkup);
 imageGallery.addEventListener('click', galleryElementClick);
@@ -24,12 +24,24 @@ function createGallery(images) {
     `;
         })
         .join('');
-    
 }
 
+
 function galleryElementClick(event) {
-    event.preventDefault();
-    console.log(event.target);
-} 
+    // imageGalleryClose.addEventListener('keydown', event);
+    // if (event.code === 'Escape') {
+    //     instance.close();
+    // }
+event.preventDefault();
+const imageEl = event.target.classList.contains('gallery__image');
+    if (!imageEl) {
+        return
+    } else {
+        const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`)
+        instance.show()
+    }
+}
 console.log(galleryItems);
 
