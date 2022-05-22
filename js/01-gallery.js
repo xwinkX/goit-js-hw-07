63eb70fe5866394d11dbc109d948352984f5f1ba
xwinkX <opalyuk.e88@ukr.net>
2022-05-22 19:00:53 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 // Change code below this line
-const imageGallery = document.querySelector('.gallery');
-// const imageGalleryClose = document.querySelector('.basicLightbox');
+const imageGallery = document.querySelector(".gallery");
+const imageGalleryClose = document.querySelector(".basicLightbox");
 const galleryMarkup = createGallery(galleryItems);
-imageGallery.insertAdjacentHTML('beforeend', galleryMarkup);
-imageGallery.addEventListener('click', galleryElementClick);
+imageGallery.insertAdjacentHTML("beforeend", galleryMarkup);
+imageGallery.addEventListener("click", galleryElementClick);
 
 function createGallery(images) {
-        return images
-        .map(({ preview, original, description }) => {
-            return `
+  return images
+    .map(({ preview, original, description }) => {
+      return `
             <div class="gallery__item">
             <a class="gallery__link" href="${original}">
             <img
@@ -22,26 +22,20 @@ function createGallery(images) {
      </a>
     </div>
     `;
-        })
-        .join('');
+    })
+    .join("");
 }
-
 
 function galleryElementClick(event) {
-    // imageGalleryClose.addEventListener('keydown', event);
-    // if (event.code === 'Escape') {
-    //     instance.close();
-    // }
-event.preventDefault();
-const imageEl = event.target.classList.contains('gallery__image');
-    if (!imageEl) {
-        return
-    } else {
-        const instance = basicLightbox.create(`
+  event.preventDefault();
+  const imageEl = event.target.classList.contains("gallery__image");
+  if (!imageEl) {
+    return;
+  } else {
+    const instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}">
-`)
-        instance.show()
-    }
+`);
+    instance.show();
+  }
 }
 console.log(galleryItems);
-
