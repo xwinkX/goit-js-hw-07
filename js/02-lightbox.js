a2281b8996edb220c510c37910a4170cda7614a2
xwinkX <opalyuk.e88@ukr.net>
2022-05-22 18:39:13 +0300

    www

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const imageGallery = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
+imageGallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGallery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 console.log(galleryItems);
